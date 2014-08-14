5a27293cc37c01f1bdbb3a1ff518b99872cf110a
2 <2@10.12.9.107>
2014-08-14 13:55:33 +0200

    random

--- HeroFactory/src/HeroSettingsLocator/RandomSettingLocator.java
+++ HeroFactory/src/HeroSettingsLocator/RandomSettingLocator.java
@@ -1,36 +1,42 @@
 package HeroSettingsLocator;
+
+import hero.HeroType;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
+import factory.IHeroSettingsLocator;
 
-public class RandomSettingLocator implements IHeroSettingLocator{
-private String losoweImie;
-private ArrayList<String> listaImion =new ArrayList<>();
+public class RandomSettingLocator implements IHeroSettingsLocator {
+	private String losoweImie;
+	private ArrayList<String> listaImion = new ArrayList<>();
 
-private ArrayList<HeroType> listaTypow =new ArrayList()<>();
+	Random r = new Random();
+	private String imie;
 
-Random r = new Random();
-private String imie;
-@Override
-public HeroType getType(){
-	listaTypow.add(hero);
-	hero=listaTypow.get(r.nextInt(listaTypow.size()+1));
-	return hero;
-	
-}
-public String getName(){
-	listaImion.add("Krzysztof_Krawczyk");
-	listaImion.add("Zbysiu_Boniek");
-	listaImion.add("Koziolek_Mato³ek");
-	listaImion.add("Stefan_Batory");
-	listaImion.add("Piotrek_Jan_Kucharowski");
-	listaImion.add("Kasia_Ga³¹Ÿ");
-	imie=listaImion.get(r.nextInt(listaImion.size()+1));
-	return imie; 
-}
-/*public String wylosujImie(){
-	losoweImie=listaImion.get(r.nextInt(listaImion.size()+1));
-	return losoweImie;
-}*/
+	@Override
+	public HeroType getType() {
+
+		HeroType[] heroes = HeroType.values();
+
+		int index = r.nextInt(heroes.length);
+		return heroes[index];
+	}
+
+	public String getName() {
+		listaImion.add("Krzysztof_Krawczyk");
+		listaImion.add("Zbysiu_Boniek");
+		listaImion.add("Koziolek_Mato³ek");
+		listaImion.add("Stefan_Batory");
+		listaImion.add("Piotrek_Jan_Kucharowski");
+		listaImion.add("Kasia_Ga³¹Ÿ");
+		imie = listaImion.get(r.nextInt(listaImion.size() + 1));
+		return imie;
+	}
+	/*
+	 * public String wylosujImie(){
+	 * losoweImie=listaImion.get(r.nextInt(listaImion.size()+1)); return
+	 * losoweImie; }
+	 */
 }
